32f263f728e3c3ce24e8b8bff66da1652fa211aa
Matthew Horridge <matthew.horridge@stanford.edu>
2014-09-02 09:09:10 -0700

    Added tests to IsConsistentAction
    For getType() and handleAction()

--- src/test/java/edu/stanford/protege/reasoning/action/IsConsistentAction_TestCase.java
+++ src/test/java/edu/stanford/protege/reasoning/action/IsConsistentAction_TestCase.java
@@ -12,6 +12,8 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 /**
  * @author Matthew Horridge, Stanford University, Bio-Medical Informatics Research Group, Date: 29/08/2014
@@ -39,6 +41,28 @@ public class IsConsistentAction_TestCase {
     public void shouldHaveSameHashCode() {
         IsConsistentAction actionB = new IsConsistentAction(kbId);
         assertThat(action.hashCode(), is(equalTo(actionB.hashCode())));
+    }
+
+    @Test
+    public void shouldReturnSameActionType() {
+        ActionType<IsConsistentHandler> typeA = action.getType();
+        ActionType<IsConsistentHandler> typeB = action.getType();
+        assertThat(typeA, is(typeB));
+    }
+
+    @Test
+    public void shouldReturnSameActionTypeForDifferentActions() {
+        ActionType<IsConsistentHandler> typeA = action.getType();
+        IsConsistentAction actionB = new IsConsistentAction(kbId);
+        ActionType<IsConsistentHandler> typeB = actionB.getType();
+        assertThat(typeA, is(typeB));
+    }
+
+    @Test
+    public void shouldDispatchToHandler() {
+        IsConsistentHandler handler = mock(IsConsistentHandler.class);
+        action.dispatch(handler);
+        verify(handler, times(1)).handleAction(action);
     }
 
     @Test(expected = NullPointerException.class)
